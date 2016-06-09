059ad9222e4682e83ce64f693e754672d37174e9
Pamela Tabak <pamelatabak@poli.ufrj.br>
2016-06-08 22:30:45 -0300

    ran wave

--- Trabalho1/Questao3.cpp
+++ Trabalho1/Questao3.cpp
@@ -44,35 +44,46 @@ void multiplyMatrixes(int** matrixA, int** matrixB, int matrixSize)
 	}
 }
 
-// void blockingMultiplyMatrixes(int** matrixA, int** matrixB, int matrixSize, int block)
-// {
-// 	int** C = new int*[matrixSize];
-// 	for (int i = 0; i < matrixSize; i++)
-// 	{
-// 		C[i] = new int[matrixSize];
-// 	}
+void blockingMultiplyMatrixes(int** matrixA, int** matrixB, int matrixSize, int block)
+{
+	// int** C = new int*[matrixSize];
+	// for (int i = 0; i < matrixSize; i++)
+	// {
+	// 	C[i] = new int[matrixSize];
+	// }
 
-// 	for (int jj = 0; jj < matrixSize; jj += block)
-// 	{
-// 		for (int ii = 0; ii < matrixSize; ii += block)
-// 		{
-// 			for (int kk = 0; kk < matrixSize; kk += block)
-// 			{
-// 				for (int j = jj; min(matrixSize, jj + b - 1); j++)
-// 				{
-// 					for (int i = ii; min(matrixSize, ii + b - 1); i++)
-// 					{
-// 						for (int k = kk; min(matrixSize, kk + b - 1); k++)
-// 						{
-// 							C[i][j] = C[i][j] + matrixA[i][k] * matrixB[k][j];
-// 							// C[i][j] = C[i][j] + matrixA[j][k] * matrixB[k][i]; // invertendo i com j
-// 						}
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
-// }
+	int **C = fillMatrix(matrixSize, 0);
+
+	int count = 0;
+	for (int jj = 0; jj < matrixSize; jj += block)
+	{
+		for (int ii = 0; ii < matrixSize; ii += block)	
+		{
+			for (int kk = 0; kk < matrixSize; kk += block)
+			{
+				for (int j = jj; j < min(matrixSize, jj + block); j++)
+				{
+					for (int i = ii; i < min(matrixSize, ii + block); i++)
+					{
+						for (int k = kk; k < min(matrixSize, kk + block); k++)
+						{
+							C[i][j] = C[i][j] + matrixA[i][k] * matrixB[k][j];
+							// C[i][j] = C[i][j] + matrixA[j][k] * matrixB[k][i]; // invertendo i com j
+						}
+					}
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < matrixSize; i++)
+	{
+		for (int j = 0; j < matrixSize; j++)
+		{
+			cout << C[i][j] << endl;
+		}
+	}
+}
 
 int main(int argc, const char* argv[])
 {
@@ -90,6 +101,7 @@ int main(int argc, const char* argv[])
 		int** matrixB = fillMatrix(matrixSize, 1);
 
 		multiplyMatrixes(matrixA, matrixB, matrixSize);
+		// blockingMultiplyMatrixes(matrixA, matrixB, matrixSize, 2);
 
 		chrono::high_resolution_clock::time_point endTime = chrono::high_resolution_clock::now();
 		chrono::duration<double> endTimeSpan = chrono::duration_cast<chrono::duration<double> >(endTime - startTime);
