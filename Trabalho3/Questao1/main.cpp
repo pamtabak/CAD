2229cfdb6e104ab580b7b3a95ff42707a8a33176
Pamela Tabak <pamelatabak@poli.ufrj.br>
2016-07-21 20:43:50 -0300

    finished question 1. started question 2

--- Trabalho3/Questao1/main.cpp
+++ Trabalho3/Questao1/main.cpp
@@ -1,31 +1,36 @@
 #include "mpi.h" // Use o arquivo de inclusao MPI apropriado.
 #include <stdio.h>
-#include <iostream>
 
 using namespace std;
 
 // Compile: mpic++ -std=c++11 main.cpp -o main.out
 // Execute: mpirun -np <#PROCESS> ./main.out
 
-/* O que fazer:
-	Use o arquivo de inclusao MPI apropriado. - OK
-	Identifique a tarefa zero como a tarefa mestre. - ??????????????
-	Inicialize o ambiente MPI. - OK
-	Obtem o numero total de tarefas.
-	Obtem a identificacao da tarefa (quem e).
-	Imprime uma mensagem Ola, que inclui a identificacao da tarefa.
-	A tarefa mestre deve imprimir o numero total de tarefas. - ????????
-	Finaliza o ambiente MPI. - OK
-*/
-
 int main(int argc, char * argv[])
 {
-	int rank, size;
-	MPI_Init(&argc, &argv);               // Inicialize o ambiente MPI.
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Obtem a identificacao da tarefa (quem e).
-	MPI_Comm_size(MPI_COMM_WORLD, &size); // Obtem a identificacao da tarefa (quem e).
-	printf("Alo mundo!! Eu sou o processo %d de %d \n", rank, size); // Imprime uma mensagem Ola, que inclui a identificacao da tarefa.
-	// cout << "Hello World" << endl;
-	MPI_Finalize(); // Finaliza o ambiente MPI.
+	
+	// Inicialize o ambiente MPI.
+	MPI_Init(&argc, &argv);
+
+	// Obtem o numero total de tarefas.
+	int size;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	// Obtem a identificacao da tarefa (quem e).
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	
+	// Imprime uma mensagem Ola, que inclui a identificacao da tarefa.
+	printf("Ola! Eu sou o processo %d. \n", rank);
+
+	// A tarefa mestre deve imprimir o numero total de tarefas.
+	if (rank == 0)
+	{
+		printf("Numero total de tarefas = %d \n", size);
+	}
+
+	// Finaliza o ambiente MPI.
+	MPI_Finalize();
+	
 	return 0;
 }
